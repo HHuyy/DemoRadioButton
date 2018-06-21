e2b263192b282f405327d6fa4d0623b79a28271d
Đừng xóa <nomnom1204@gmail.com>
2018-06-21 07:53:41 +0700

    init

--- DemoRadioButton/ViewController.swift
+++ DemoRadioButton/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var radioButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +19,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        radioButtons.forEach { (button) in
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        print(sender.currentTitle ?? "Du")
+    }
 }
 
